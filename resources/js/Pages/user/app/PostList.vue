d36fb118e524015fc065c5fd37b8bcfce6e0a8ab
junaeidy <junaeidys0@gmail.com>
2024-09-22 09:54:23 +0700

    Implement editing post attachments: Removing and adding new ones

--- resources/js/Pages/user/app/PostList.vue
+++ resources/js/Pages/user/app/PostList.vue
@@ -2,6 +2,7 @@
 import PostItems from './PostItems.vue';
 import PostModal from './PostModal.vue';
 import {ref} from "vue";
+import {usePage} from "@inertiajs/vue3";
 
 defineProps({
     posts: Array
@@ -13,48 +14,12 @@ function openEditModal(post) {
     editPost.value = post;
     showEditModal.value = true;
 }
-const post1 = {
-    user: {
-        id: 1,
-        avatar: 'https://randomuser.me/api/portraits/men/46.jpg',
-        name: 'Junaeidy Setiawan'
-    },
-    group: null,
-    attachments: [
-        {
-            id: 1,
-            name: 'test.png',
-            url: 'https://picsum.photos/1000',
-            mime: 'image/png'
-        },
-        {
-            id: 2,
-            name: 'test2.png',
-            url: 'https://picsum.photos/1000',
-            mime: 'image/png'
-        },
-        {
-            id: 3,
-            name: 'MyDocument.docx',
-            url: '#',
-            mime: 'application/msword'
-        },
-    ],
-    body: '<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis ullam, aspernatur obcaecati aliquid repudiandae quaerat, earum quae, exercitationem deserunt non laudantium quis placeat eligendi fugit molestiae. Quaerat fugiat quae dicta.</p>',
-    created_at: '2024-06-26 23:50'
-}
-const post2 = {
-    user: {
-        id: 1,
-        avatar: 'https://randomuser.me/api/portraits/lego/4.jpg',
-        name: 'Siti Rahma Br Sitorus'
-    },
-    group: {
-        id: 1,
-        name: 'Fisika Dasar 2'
-    },
-    body: '<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis ullam, aspernatur obcaecati aliquid repudiandae quaerat, earum quae, exercitationem deserunt non laudantium quis placeat eligendi fugit molestiae. Quaerat fugiat quae dicta. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis ullam, aspernatur obcaecati aliquid repudiandae quaerat, earum quae, exercitationem deserunt non laudantium quis placeat eligendi fugit molestiae. Quaerat fugiat quae dicta.</p>',
-    created_at: '2024-06-26 23:59'
+function onModalHide() {
+    editPost.value = {
+        id: null,
+        body: '',
+        user: authUser
+    }
 }
 </script>
 
@@ -62,7 +27,7 @@ const post2 = {
     <div class="overflow-auto">
         <PostItems v-for="post of posts" :key="post.id" :post="post" @editClick="openEditModal"/>
 
-        <PostModal :post="editPost" v-model="showEditModal"/>
+        <PostModal :post="editPost" v-model="showEditModal" @hide="onModalHide"/>
     </div>
 </template>
 
