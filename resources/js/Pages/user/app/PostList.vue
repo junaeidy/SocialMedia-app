bfe91239f33dc73b90e39baefae8f00f1731fb78
junaeidy <junaeidys0@gmail.com>
2024-09-26 23:14:29 +0700

    Implement preview of the attachments

--- resources/js/Pages/user/app/PostList.vue
+++ resources/js/Pages/user/app/PostList.vue
@@ -1,4 +1,5 @@
 <script setup>
+import AttachmentPreviewModal from './AttachmentPreviewModal.vue'
 import PostItems from './PostItems.vue';
 import PostModal from './PostModal.vue';
 import {ref} from "vue";
@@ -7,12 +8,21 @@ import {usePage} from "@inertiajs/vue3";
 defineProps({
     posts: Array
 })
-
+const authUser = usePage().props.auth.user;
 const showEditModal = ref(false)
+const showAttachmentsModal = ref(false)
 const editPost = ref({})
+const previewAttachmentsPost = ref({})
 function openEditModal(post) {
     editPost.value = post;
     showEditModal.value = true;
+}
+function openAttachmentPreviewModal(post, index) {
+    previewAttachmentsPost.value = {
+        post,
+        index
+    }
+    showAttachmentsModal.value = true;
 }
 function onModalHide() {
     editPost.value = {
@@ -25,9 +35,15 @@ function onModalHide() {
 
 <template>
     <div class="overflow-auto">
-        <PostItems v-for="post of posts" :key="post.id" :post="post" @editClick="openEditModal"/>
+        <PostItems v-for="post of posts" :key="post.id" :post="post"
+                  @editClick="openEditModal"
+                  @attachmentClick="openAttachmentPreviewModal"
+        />
 
-        <PostModal :post="editPost" v-model="showEditModal" @hide="onModalHide"/>
+
+        <AttachmentPreviewModal :attachments="previewAttachmentsPost.post?.attachments || []"
+                                v-model:index="previewAttachmentsPost.index"
+                                v-model="showAttachmentsModal"/>
     </div>
 </template>
 
