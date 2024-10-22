fa2f851f9813cd91d64878b4826ca47cb1bbf3f5
junaeidy <junaeidys0@gmail.com>
2024-10-23 01:34:03 +0700

    Install Laravel telescope

--- resources/js/Pages/user/app/GroupItems.vue
+++ resources/js/Pages/user/app/GroupItems.vue
@@ -8,7 +8,7 @@ import {Link} from '@inertiajs/vue3'
 <template>
     <div class="cursor-pointer hover:bg-gray-100">
         <Link :href="route('group.profile', group.slug)" class="flex items-start gap-1 py-2 px-2">
-        <img :src="group.thumbnail_url" class="w-[32px] rounded-full"/>
+        <img :src="group.thumbnail_url" class="w-[32px] h-[32px] rounded-full"/>
             <div class="flex-1">
                 <div class="flex justify-between">
                     <h3 class="font-black text-lg">{{ group.name }}</h3>
